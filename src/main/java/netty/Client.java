b4e02030214dfe0ee3e514b560a61b9a6cae9c08
soy-braised-pork <2428784410@qq.com>
2021-04-13 22:04:24 +0800

    upadte

--- src/main/java/netty/Client.java
+++ src/main/java/netty/Client.java
@@ -1,4 +1,42 @@
 package netty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.nio.channels.SocketChannel;
+
+
 public class Client {
+    public static void main(String[] args) {
+        //客户端NIO线程组
+        EventLoopGroup group=new NioEventLoopGroup();
+        try {
+            //客户端启动类
+            Bootstrap b=new Bootstrap();
+            //创建默认的channel、handler、PIPline，用于调度和执行网络事件
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                            throws Exception{
+                            ch.pipline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            //发起异步连接操作
+            ChannelFuture f=b.connect(host,port).sync();
+
+            //等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        }finally {
+            //优雅退出，释放NIO线程组
+            group.shutdownGracefully();
+        }
+    }
 }
--- src/main/java/netty/HttpServer.java
+++ src/main/java/netty/HttpServer.java
@@ -9,7 +9,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 public class HttpServer {
     public static void main(String[] args) {
         /*
-        bossgroup、workergroup默认线程数为CPU核心乘以2
+        bossgroup、workergroup默认线程数为CPU内核数的2倍
         bossgroup用于接受客户端发来的请求，接收到请求后将后续操作交由workergroup来处理
          */
         //两个线程组
@@ -21,7 +21,7 @@ public class HttpServer {
             channel方法用于指定服务器监听套接字通道NioServerSocketChannel，其内部管理了一个java nio中的ServerSocketChannel实例
             channelHandler方法用于设置业务职责链，任链具体是什么，它其实就是由一个个的 ChannelHandler 串联而成，形成的链式结构。正是这一个个的 ChannelHandler 帮我们完成了要处理的事情。
              */
-            //服务端启动辅助类 bootstrap
+            //服务端启动辅助类 serverbootstrap
             ServerBootstrap bootstrap=new ServerBootstrap();
             //
             bootstrap.group(bossGroup,workerGroup)
@@ -30,6 +30,7 @@ public class HttpServer {
 
             /*
             bootstrap的bind方法将服务绑定在8080端口，bind方法内部会执行端口绑定等一系列操作，使前面的配置各司其职
+            加锁----->同步
              */
             ChannelFuture future=bootstrap.bind(8080).sync();
 
